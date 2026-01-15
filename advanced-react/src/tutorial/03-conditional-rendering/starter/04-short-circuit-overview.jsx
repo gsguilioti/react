368e12fda036cba8fb422e8420c1809fc5d1f1ad
gsguilioti <gsguilioti@gmail.com>
2026-01-15 19:50:23 -0300

    toggle

--- advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState('');
+  const [name, setName] = useState('eren');
+
+
+
+  return <div>
+    {}
+    <h4>falsy or : {text || 'hello world'}</h4>
+    <h4>falsy and : {text && 'hello world'}</h4>
+    <h4>truthy or : {name || 'hello world'}</h4>
+    <h4>truthy and : {name && 'hello world'}</h4>
+  </div>;
 };
 export default ShortCircuitOverview;
