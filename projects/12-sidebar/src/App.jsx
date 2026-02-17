b8954622ecaa7885e2978dac3eeed3f9a7f8d6ed
gsguilioti <gsguilioti@gmail.com>
2026-02-17 16:07:47 -0300

    sidebar project

---
+++ projects/12-sidebar/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./assets/Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+
+const App = () => {
+  return <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>;
+};
+export default App;
