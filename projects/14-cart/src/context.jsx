b3f813fadcc58bf0fc8fff5a60dbb865799a1678
gsguilioti <gsguilioti@gmail.com>
2026-02-25 16:05:04 -0300

    cart project map

---
+++ projects/14-cart/src/context.jsx
@@ -0,0 +1,26 @@
+import {useContext, useReducer, useEffect, createContext } from 'react'
+import reducer from './reducer'
+import { CLEAR_CART, REMOVE, DECREASE, INCREASE, LOADING, DISPLAY_ITEMS } from './actions'
+import cartItems from './data'
+
+const AppContext = createContext()
+
+const initialState = {
+    loading: false,
+    cart: new Map(cartItems.map((item)=> [item.id, item]))
+}
+
+export const AppProvider = ({children}) => {
+    
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <AppContext.Provider value={{...state}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
---
+++ projects/14-cart/src/reducer.js
@@ -0,0 +1,7 @@
+import { CLEAR_CART, REMOVE, DECREASE, INCREASE, LOADING, DISPLAY_ITEMS } from './actions'
+
+const reducer = (state, action) => {
+    return state
+}
+
+export default reducer
